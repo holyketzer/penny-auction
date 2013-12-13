c47ccf7d4f0e58a9fb7c250cd8836f979c961b3f
Alex Emelyanov <holyketzer@gmail.com>
2013-12-13 11:42:15 +0400

    Code format refactoring of models and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ActiveRecord::Base
-	validates :name, :presence => true
+	validates :name, :description, :shop_price, presence: true
 	validates :name, uniqueness: { case_sensitive: false }
-  validates :description, :presence => true
-  validates :shop_price, :presence => true
   validates :shop_price, numericality: { greater_than: 0.01 }
   validates :shop_price, fractionality: { greater_than: 0.01 }  
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,9 @@
 require 'spec_helper'
 
 describe Category do
-	describe "name" do		
+	describe "validations" do		
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of(:name).case_insensitive }
-  end
-
-  describe "description" do
   	it { should validate_presence_of :description }
-  end
-
-  context "ancestry" do
-  	# TODO: implement ancestry testing
-  	# it { should belong_to(:category) }
   end
 end
