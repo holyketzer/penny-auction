f5d3b5bb649f9e89b46712d16202258fa74d2936
Alex Emelyanov <holyketzer@gmail.com>
2013-12-15 16:39:22 +0400

    Add specs for fractionality validator

--- app/validators/fractionality_validator.rb
+++ app/validators/fractionality_validator.rb
@@ -1,23 +1,66 @@
 class FractionalityValidator < ActiveModel::EachValidator
-  CHECKS = { greater_than: :>, greater_than_or_equal_to: :>=,
-                 equal_to: :==, less_than: :<, less_than_or_equal_to: :<=,
-                 other_than: :!= }.freeze
 
-  def validate_each(record, attribute, value)
-    if value
-      value = value.modulo(1)
+  def validate_each(record, attr_name, value)
+    before_type_cast = :"#{attr_name}_before_type_cast"
 
-      options.slice(*CHECKS.keys).each do |option, option_value|        
-        case option_value
+    raw_value = record.send(before_type_cast) if record.respond_to?(before_type_cast)
+    raw_value ||= value
+
+    return if options[:allow_nil] && raw_value.nil?
+      
+    unless raw_value = parse_raw_value_as_a_float_number(raw_value)
+      record.errors[attr_name] << (options[:message] || "should be value with floating point")
+      return
+    end
+
+    frac_match = /[\.,]\d+/.match(raw_value.to_s)    
+    if frac_match
+      raw_value = Kernel.Float(frac_match[0])
+
+      multiplier = options[:multiplier]
+      if raw_value != 0 and multiplier      
+        case multiplier
         when Proc
-          option_value = option_value.call(record)
+          multiplier = multiplier.call(record)
         when Symbol
-          option_value = record.send(option_value)
+          multiplier = record.send(multiplier)
         end
+        
+        if raw_value < multiplier || raw_value.to_s.size > multiplier.to_s.size
+          record.errors[attr_name] << (options[:message] || "should be multiplication of #{multiplier}")
+        end
+      end
+    end
+  end
 
-        unless value.send(CHECKS[option], option_value)
-          record.errors[attribute] << (options[:message] || "fractional part should be #{option} #{option_value}")
+  protected
+    def parse_raw_value_as_a_float_number(raw_value)
+      return raw_value if raw_value.is_a? Float or raw_value.is_a? Rational
+      return nil if raw_value.is_a? Integer
+
+      if raw_value !~ /\A0[xX]/ and Kernel.Float(raw_value)
+        begin
+          nil if Kernel.Integer(raw_value, 10)
+        rescue ArgumentError, TypeError
+          Kernel.Float(raw_value)
         end
+      end
+    rescue ArgumentError, TypeError
+      nil
+    end   
+
+    def filtered_options(value)
+      filtered = options.dup
+      filtered[:value] = value
+      filtered
+    end
+end
+
+module ActiveModel
+  module Validations
+    module HelperMethods
+      def validates_fractionality_of(*attr_names)
+        validates_with FractionalityValidator, _merge_attributes(attr_names)
       end
     end
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ describe Product do
     it { should validate_uniqueness_of(:name).case_insensitive }
     it { should validate_presence_of :description }
     it { should validate_presence_of :shop_price }
-    it { should validate_numericality_of(:shop_price).is_greater_than(0.01) }
+    it { should validate_numericality_of(:shop_price).is_greater_than_or_equal_to(0.01) }
     it { should validate_min_fractionality_of(:shop_price, 0.01) }
   end
 
