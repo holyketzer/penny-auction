f588e30d2a1d8037c0dd79fc5ab400a319233ee9
Alex Emelyanov <holyketzer@gmail.com>
2014-01-19 18:27:03 +0400

    Add user registration spec

---
+++ spec/acceptance/user_account_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+feature 'User login', %q{  
+  As an user
+  I want to login into auction
+  In order to take part in auctions
+ } do
+
+  let(:user) { create(:user) }
+
+  background do
+    visit root_path
+  end
+
+  context 'for unregistered user' do
+    scenario 'Unauthenticated user can login or create account' do
+      expect(page).to have_content 'Текущие аукционы'
+      expect(page).to have_link 'Вход'
+      expect(page).to have_link 'Регистрация'
+    end
+
+    scenario 'User creates new account' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'user@mail.com'
+      fill_in 'Пароль', with: 'secret'
+      fill_in 'Подтверждение пароля', with: 'secret'
+      click_on 'Зарегистрироваться'
+
+      expect(current_path).to eq(root_path)    
+      expect(page).to have_link 'user@mail.com'
+      expect(page).to have_link 'Выход'
+    end
+  end
+
+  context 'for registered user' do
+    background do
+      click_on 'Вход'
+      sign_in_with user.email, user.password
+    end
+
+    scenario 'login' do
+      expect(current_path).to eq(root_path)    
+      expect(page).to have_link user.email
+      expect(page).to have_link 'Выход'
+      expect(page).to have_content 'Текущие аукционы'
+      expect(page).to have_content 'Вход выполнен'    
+    end
+
+    scenario 'logout' do    
+      click_on 'Выход'
+
+      expect(page).to have_link 'Вход'
+      expect(page).to have_content 'Выход выполнен'
+    end
+  end
+end
